8cc5d3c89a3aa0da71d157a4cc3e01aeeb104732
lubien <lubien1996@gmail.com>
2021-09-25 21:42:07 -0300

    Install graphql client

--- config/config.exs
+++ config/config.exs
@@ -49,3 +49,7 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+config :fly_together, :graphql_endpoint, "https://api.fly.io/graphql"
+
+config :fly_together, :client_api, Fly.ClientLive
